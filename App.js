b188e91ef106775d240a04ca48405f3173b2dfbd
ChristopherMorales <Christopher.Morales4752@gmail.com>
2021-03-27 11:56:56 -0400

    Finish Side Menu

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import MainTabScreen from './screens/MainTabScreen';
+import {DrawerContent} from './screens/DrawerContent';
+import SupportScreen from './screens/SupportScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import BookmarkScreen from './screens/BookmarkScreen';
 
 
 const Drawer = createDrawerNavigator();
@@ -10,9 +14,12 @@ const Drawer = createDrawerNavigator();
 const App = () => {
   return(
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={MainTabScreen} />
-        {/* <Drawer.Screen name="Details" component={DetailsStackScreen} /> */}
+      <Drawer.Navigator drawerContent= {props => <DrawerContent {...props} />}>
+        <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
+        <Drawer.Screen name="SupportScreen" component={SupportScreen} />
+        <Drawer.Screen name="SettingScreen" component={SettingsScreen} />
+        <Drawer.Screen name="BookmarkScreen" component={BookmarkScreen} />
+        
       </Drawer.Navigator>
       {/* <Stack.Navigator screenOptions = {{
         headerStyle: {
